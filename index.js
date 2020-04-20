6832c3f197276649537da40b578527e4d9206282
tygedavis <tyge.davis@gmail.com>
2020-04-20 14:59:43 -0600

    Console logs list of videos on submit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
@@ -16,3 +16,7 @@ app.on('ready', () => {
 
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 });
+
+ipcMain.on('videos:added', (event, videos) => {
+  console.log(videos)
+})
