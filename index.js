92535ac13615a8e968aca40ccc8d589feb0f887e
tygedavis <tyge.davis@gmail.com>
2020-04-20 15:12:59 -0600

    Console logs a list of multiple video selections

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
@@ -18,5 +20,22 @@ app.on('ready', () => {
 });
 
 ipcMain.on('videos:added', (event, videos) => {
-  console.log(videos)
-})
+  // const promise = new Promise((resolve, reject) => {
+  //   ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
+  //     resolve(metadata);
+  //   });
+  // });
+
+  // promise.then((metadata) => { console.log(metadata) });
+
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve(metadata);
+      });
+    });
+  });
+
+  Promise.all(promises)
+    .then((results) => console.log(results));
+});
