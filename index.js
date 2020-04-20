8afc885e368bd3639fbf97c1656df5c1a6ddbe75
tygedavis <tyge.davis@gmail.com>
2020-04-20 16:04:12 -0600

    Converted files can now be shown in folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 const _ = require('lodash');
 
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain, shell } = electron;
 
 let mainWindow;
 
@@ -44,9 +44,16 @@ ipcMain.on('conversion:start', (event, videos) => {
 
     ffmpeg(video.path)
       .output(outputPath)
+      .on('progress', ({ timemark }) => {
+        mainWindow.webContents.send('conversion:progress', { video, timemark })
+      })
       .on('end', () => {
         mainWindow.webContents.send('conversion:end', { video: video, outputPath: outputPath })
       })
       .run();
   });
+});
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -19,11 +19,15 @@ export const convertVideos = () => (dispatch, getState) => {
   ipcRenderer.on('conversion:end', (event, { video, outputPath }) => {
     dispatch({ type: VIDEO_COMPLETE, payload: { ...video, outputPath }})
   });
+
+  ipcRenderer.on('conversion:progress', (event, { video, timemark }) => {
+    dispatch({ type: VIDEO_PROGRESS, payload: { ...video, timemark }})
+  })
 };
 
 // TODO: Open the folder that the newly created video exists in
 export const showInFolder = outputPath => dispatch => {
-
+  ipcRenderer.send('folder:open', outputPath);
 };
 
 export const addVideo = video => {
